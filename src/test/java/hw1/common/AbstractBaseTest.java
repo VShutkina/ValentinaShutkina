074325bf7a97a44832da10e2f2ea2ecb4b3529bc
shutkina87 <valentina.shutkina@icloud.com>
2019-11-18 20:13:51 +0300

    homework 1 - AbstractBaseTest changed (abstract was added)

--- src/test/java/hw1/common/AbstractBaseTest.java
+++ src/test/java/hw1/common/AbstractBaseTest.java
@@ -5,7 +5,7 @@ import com.epam.tat.module4.Calculator;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class AbstractBaseTest {
+public abstract class AbstractBaseTest {
 
     protected Calculator calculator;
 
