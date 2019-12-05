756294bd704a916a8adf84831abd88bb68aa3899
shutkina87 <valentina.shutkina@icloud.com>
2019-12-06 00:28:06 +0300

    homework 5 - second jenkins commit

---
+++ src/test/java/hw5/common/AbstractBaseTest.java
@@ -0,0 +1,47 @@
+package hw5.common;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public abstract class AbstractBaseTest {
+
+    protected WebDriver driver;
+    protected Properties testProperties;
+
+    @BeforeSuite
+    public void suiteSetUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20000, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().setScriptTimeout(30000, TimeUnit.MILLISECONDS);
+
+        testProperties = new Properties();
+        try {
+            testProperties.load(new FileInputStream("src/main/resources/user.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            driver.close();
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+}
--- src/test/java/hw5/steps/HomePageActionSteps.java
+++ src/test/java/hw5/steps/HomePageActionSteps.java
@@ -1,6 +1,5 @@
-package hw3.steps;
+package hw5.steps;
 
-import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
 public class HomePageActionSteps extends AbstractSteps {
@@ -9,12 +8,10 @@ public class HomePageActionSteps extends AbstractSteps {
         super(driver);
     }
 
-    @Step("I open EPAM Jdi Site")
     public void openEpamJdiSite() {
         homePage.open();
     }
 
-    @Step("I login as '{0}' with password '{1}'")
     public void login(final String username, final String password) {
         homePage.userIconClick();
         homePage.setTextUsernameTextField(username);
@@ -22,27 +19,22 @@ public class HomePageActionSteps extends AbstractSteps {
         homePage.loginButtonClick();
     }
 
-    @Step("I switch to iframe")
     public void switchToIframe() {
         homePage.switchToIframe();
     }
 
-    @Step("I switch to main window")
     public void switchToOriginalWindow() {
         homePage.switchToOriginalWindow();
     }
 
-    @Step("I click on Header menu item {0}")
     public void clickHeaderMenuItem(final String headerMenuItem) {
         homePage.getHeaderMenu().headerMenuItemClick(headerMenuItem);
     }
 
-    @Step("I click on {0} Menu Item in Lest Side")
     public void clickLeftSideMenuItem(final String leftSideMenuItem) {
         homePage.getLeftSide().leftPanelMenuItemsClick(leftSideMenuItem);
     }
 
-    @Step("I click on {0} Header Menu Sub Item")
     public void clickHeaderSubMenuItem(final String headerSubMenuItem) {
         homePage.getHeaderMenu().headerSubMenuItemClick(headerSubMenuItem);
     }
