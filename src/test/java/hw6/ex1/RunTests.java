5136601f47236182f616625cecc212e94d9cc6ca
shutkina87 <valentina.shutkina@icloud.com>
2019-12-09 18:36:25 +0300

    homework 6 - tests suites added

--- src/test/java/hw6/ex1/RunTests.java
+++ src/test/java/hw6/ex1/RunTests.java
@@ -3,6 +3,6 @@ package hw6.ex1;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "classpath:hw6/ex1")
+@CucumberOptions(features = "./src/test/resources/hw6/ex1")
 public class RunTests extends AbstractTestNGCucumberTests {
 }
